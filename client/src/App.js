d08293d45daf50e7b953543be04095a58f6c374d
Brandon Haley <kyle7286@gmail.com>
2022-01-27 21:16:43 -0500

    Setup homepage routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Infra Imports
+import { BrowserRouter as Router, Route, Routes, Redirect } from "react-router-dom";
+
+// Page Imports
+import Home from "./pages/Home";
+
+// Component Imports
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div data-component="DivInRouter" className="my-0">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+      </div>
+    </Router >
   );
+
+
+
+
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ client/src/components/Home/index.js
@@ -0,0 +1,12 @@
+function HomeComponent() {
+
+    /* ---------------------------- Component Render ---------------------------- */
+    return (
+            <div>    
+                <h1>Home Page</h1>
+            </div>
+    );
+}
+
+// Exported for import within app.js
+export default HomeComponent;
