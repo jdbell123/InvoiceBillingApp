4b99f3f23e68053ab9794fb46735cfe53a1a6fac
John Bell <jdbell123@gmail.com>
2022-02-27 07:34:09 -0500

    add Outlet to fix wrapper issue. Also - added Header component

--- client/src/components/Wrapper/index.js
+++ client/src/components/Wrapper/index.js
@@ -1,15 +1,24 @@
+import { Outlet } from 'react-router-dom';
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 
+import Header from "../Header";
 import SideBar from "../SideBar";
 
 function Wrapper() {
+
     return (
-        <main className="d-inline-block bg-dark vh-100" >
+        <main className="d-inline-block bg-light vh-100" >
+            <Row>
+                <Header />
+            </Row>
             <Row>
                 <Col>
                     <SideBar />
+                </Col>
+                <Col>
+                    <Outlet />
                 </Col>
             </Row>
         </main>
