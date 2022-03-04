66e0b74c64454c6429b2dd025f332d09d30bcf11
John Bell <jdbell123@gmail.com>
2022-03-03 21:56:38 -0500

    lined up code in file

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -9,18 +9,18 @@ function SideBar() {
     return (
         <ul className="nav nav-fill flex-column mb-auto text-blue">
             <li className="nav-item px-0 my-2 text-center" href="/">
-                                            <NavLink to="/" exact={true} className="nav-link ps-2">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
-                                                    <path fillRule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
-                                                    <path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
-                                                </svg>
-                                                <div>
-                                                    Home
-                                            </div>
-                                            </NavLink>
-                                        </li>
-                                        <hr className="m-0" />
-                                                                    
+                <NavLink to="/" exact={true} className="nav-link ps-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                        <path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+                    </svg>
+                    <div>
+                        Home
+                    </div>
+                </NavLink>
+            </li>
+            <hr className="m-0" />
+
             <li className="nav-item">
                 <a href="#" className="nav-text nav-link active" aria-current="page">
                     Set Processing Dates
@@ -106,16 +106,16 @@ function SideBar() {
             <hr className="m-0" />
 
             <li className=" nav-item my-2 text-center">
-                                            <NavLink to="/agent" className="nav-link ps-2">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                                    <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                                                </svg>
-                                                <div>
-                                                    Agent
-                                            </div>
-                                            </NavLink>
-                                        </li>
+                <NavLink to="/agent" className="nav-link ps-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                    </svg>
+                    <div>
+                        Agent
+                    </div>
+                </NavLink>
+            </li>
             <hr className="m-0" />
         </ul>
     )
